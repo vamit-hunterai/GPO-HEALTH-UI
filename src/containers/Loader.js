c786cf4e72f0bfb375484fce308835eab34088d9
vamit-hunterai <121946606+vamit-hunterai@users.noreply.github.com>
2023-09-11 20:27:58 +0530

    dep change

--- src/containers/Loader.js
+++ src/containers/Loader.js
@@ -6,7 +6,7 @@
 */
 
 import React from 'react';
-import { css } from "@emotion/core";
+import { css } from "@emotion/react";
 import ClipLoader from "react-spinners/ClipLoader";
 
 const override = css`
